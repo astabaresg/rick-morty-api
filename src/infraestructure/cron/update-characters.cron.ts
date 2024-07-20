680c8b99c3c8c15c39d28c77a8a6a07bdca3b29d
Alvaro Sebastian Tabares <astg0616@gmail.com>
2024-07-19 21:21:29 -0500

    Cron updated
    Cron updated to run every 12h

--- src/infraestructure/cron/update-characters.cron.ts
+++ src/infraestructure/cron/update-characters.cron.ts
@@ -5,7 +5,7 @@ import { updateCharactersInDatabase } from "../services/character.service";
 // "0 */12 * * *" // Every 12 hours
 // Every 5 minutes cron.schedule("*/5 * * * *", async () => {
 
-cron.schedule("*/1 * * * *", async () => {
+cron.schedule("0 */12 * * *", async () => {
   logger.info("Running the character update job");
   await updateCharactersInDatabase();
 });
