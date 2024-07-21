642f9e008aea7867262e4ab59ad0459341c48f14
Alvaro Sebastian Tabares <astg0616@gmail.com>
2024-07-21 17:56:15 -0500

    Console log changed for logger

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,6 +1,7 @@
 import { createClient } from "redis";
 import dotenv from "dotenv";
 import { envs } from "./envs";
+import logger from "./logger";
 
 dotenv.config();
 
@@ -8,6 +9,6 @@ const redisClient = createClient({
   url: `redis://${envs.REDIS_HOST}:${envs.REDIS_PORT}`,
 });
 
-redisClient.on("error", (err) => console.log("Redis Client Error", err));
+redisClient.on("error", (err) => logger.error("Redis Client Error", err));
 
 export default redisClient;
